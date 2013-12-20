48e5ffd101ee22f4457f6236afc54dbba7e56e7d
ptwoms <ptwoms@gmail.com>
2013-12-21 00:23:18 +0800

    Initialize Git

---
+++ P2MSStoryboardTests/P2MSStoryboardTests.m
@@ -0,0 +1,34 @@
+//
+//  P2MSStoryboardTests.m
+//  P2MSStoryboardTests
+//
+//  Created by PYAE PHYO MYINT SOE on 21/12/13.
+//  Copyright (c) 2013 PYAE PHYO MYINT SOE. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface P2MSStoryboardTests : XCTestCase
+
+@end
+
+@implementation P2MSStoryboardTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
